f1d60b1be0bb12a9e23abe26c049ac92be15b5ea
petergaobro <37321872+petergaobro@users.noreply.github.com>
2024-07-10 14:55:19 +1000

    add fallback for latestInvoice

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,10 +5,10 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 export default async function Page() {
     // const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+    // const latestInvoices = await fetchLatestInvoices();
     const
         {
             totalPaidInvoices,
@@ -38,7 +38,10 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                <LatestInvoices latestInvoices={latestInvoices} />
+                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
